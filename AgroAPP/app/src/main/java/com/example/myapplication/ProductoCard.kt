3d0cdc77b904ed03344ffb0853d1747be532ac02
FrankMolina17 <franklinmoloina12172004@gmail.com>
2026-09-23 15:19:30 -0500

    docs: agregar funciones @Preview a ProductoCard y ProductoCatalogoScreen

--- AgroAPP/app/src/main/java/com/example/myapplication/ProductoCard.kt
+++ AgroAPP/app/src/main/java/com/example/myapplication/ProductoCard.kt
@@ -20,6 +20,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
@@ -94,4 +95,13 @@ fun ProductoCard(
             }
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ProductoCardPreview() {
+    ProductoCard(
+        producto = productosDummy.first(),
+        onClick = {}
+    )
 }
--- AgroAPP/app/src/main/java/com/example/myapplication/ProductoCatalogoScreen.kt
+++ AgroAPP/app/src/main/java/com/example/myapplication/ProductoCatalogoScreen.kt
@@ -14,6 +14,7 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 
@@ -53,4 +54,14 @@ fun ProductoCatalogoScreen(
             }
         }
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun ProductoCatalogoScreenPreview() {
+    ProductoCatalogoScreen(
+        productos = productosDummy,
+        onProductoClick = {},
+        onBackClick = {}
+    )
 }
